6e9ccca759a41013e264405c2c552b64238b0d85
RoninsWork <yurkaronin@gmail.com>
2022-11-18 16:46:45 +0300

    Начал разметку секции услуг на главной

--- home.php
+++ home.php
@@ -18,6 +18,7 @@
   <?php include './components/header.php';?>
 
   <main>
+    <!-- первый экран  -->
     <section class="first">
       <div class="wrapper">
         <div class="first__flex">
@@ -40,8 +41,52 @@
 
         </div>
 
+      </div>
+    </section>
 
+    <section class="services">
+      <div class="wrapper">
+        <h2 class="visually-hidden">Список услуг</h2>
 
+        <ul class="services__list">
+
+          <li class="services__item accordion">
+            <button class="accordion__top active" type="button">
+              <svg width="10" height="10" viewBox="0 0 10 10" fill="none" xmlns="http://www.w3.org/2000/svg">
+                <path d="M1 4L5 8M5 8L9 4M5 8L5 0" stroke="#00F0C0" stroke-width="2" />
+              </svg>
+              <h3 class="accordion__title">Идеи по рынку</h3>
+            </button>
+            <div class="accordion__body">
+
+              <h3 class="title">Идеи по рынку</h3>
+              <p class="lead">Прямой доступ на биржевой валютный рынок. Покупка акций, облигаций, еврооблигаций, срочных
+                контрактов.</p>
+              <hr>
+              <p class="text">Пример описания услуги, полно но не очень длинно. Пример описания услуги, полно но не
+                очень длинно буквально пару строк.</p>
+
+              <div class="buttons">
+
+                <a class="button" href="#">
+                  <span>подробнее</span>
+                  <svg width="10" height="10" viewBox="0 0 10 10" fill="none" xmlns="http://www.w3.org/2000/svg">
+                    <path d="M4.5 9L8.5 5M8.5 5L4.5 1M8.5 5L0.5 5" stroke="#1B1E1E" stroke-width="2" />
+                  </svg>
+                </a>
+
+                <a class="button button--transparent" href="#">
+                  <span>Все услуги</span>
+                  <svg width="10" height="10" viewBox="0 0 10 10" fill="none" xmlns="http://www.w3.org/2000/svg">
+                    <path d="M4.5 9L8.5 5M8.5 5L4.5 1M8.5 5L0.5 5" stroke="#00F0C0" stroke-width="2" />
+                  </svg>
+                </a>
+
+              </div>
+
+            </div>
+          </li>
+        </ul>
       </div>
     </section>
   </main>
