dc053f38e568da7382d83bdfa3d73371989be97b
RoninsWork <yurkaronin@gmail.com>
2022-11-21 19:33:29 +0300

    Правки под пиксель перфект

--- home.php
+++ home.php
@@ -49,7 +49,7 @@
         <h2 class="visually-hidden">Список услуг</h2>
 
         <ul class="services__list">
-
+        <!-- Идеи по рынку  -->
           <li class="services__item accordion">
 
             <div class="accordion__top">
@@ -99,6 +99,57 @@
 
             </div>
           </li>
+          <!-- Автоследование  -->
+          <li class="services__item accordion">
+
+            <div class="accordion__top">
+              <svg width="10" height="18" viewBox="0 0 10 18" fill="none" xmlns="http://www.w3.org/2000/svg">
+                <path d="M1 12L5 16M5 16L9 12M5 16L5 8" stroke="#00F0C0" stroke-width="2" />
+                <path class="line" d="M5 8L5 0" stroke="#00F0C0" stroke-width="2" />
+              </svg>
+              <h3 class="accordion__title">Автоследование</h3>
+            </div>
+
+            <div class="accordion__body">
+              <div class="services__item-grid">
+                <div class="services__item-left">
+                  <h3 class="services__subtitle">Автоследование</h3>
+                  <p class="services__lead">Прямой доступ на биржевой валютный рынок. Покупка акций, облигаций,
+                    еврооблигаций,
+                    срочных
+                    контрактов.</p>
+                  <p class="services__text">Пример описания услуги, полно но не очень длинно. Пример описания услуги,
+                    полно но не
+                    очень длинно буквально пару строк.</p>
+
+                  <div class="services__buttons">
+
+                    <a class="button button--icon" href="#">
+                      <span>подробнее</span>
+                      <svg width="10" height="10" viewBox="0 0 10 10" fill="none" xmlns="http://www.w3.org/2000/svg">
+                        <path d="M4.5 9L8.5 5M8.5 5L4.5 1M8.5 5L0.5 5" stroke="#1B1E1E" stroke-width="2" />
+                      </svg>
+                    </a>
+
+                    <a class="button button--transparent button--icon" href="#">
+                      <span>Все услуги</span>
+                      <svg width="10" height="10" viewBox="0 0 10 10" fill="none" xmlns="http://www.w3.org/2000/svg">
+                        <path d="M4.5 9L8.5 5M8.5 5L4.5 1M8.5 5L0.5 5" stroke="#00F0C0" stroke-width="2" />
+                      </svg>
+                    </a>
+
+                  </div>
+                </div>
+                <div class="services__item-right">
+                  <img src="./img/services/2.jpg" alt="картинка услуги" width="808">
+                </div>
+              </div>
+
+
+
+            </div>
+          </li>
+          <!-- База знаний  -->
           <li class="services__item accordion">
 
             <div class="accordion__top">
@@ -140,7 +191,7 @@
                   </div>
                 </div>
                 <div class="services__item-right">
-                  <img src="./img/services/2.jpg" alt="картинка услуги" width="808">
+                  <img src="./img/services/3.jpg" alt="картинка услуги" width="808">
                 </div>
               </div>
 
@@ -148,6 +199,7 @@
 
             </div>
           </li>
+          <!-- Вебинары  -->
           <li class="services__item accordion">
 
             <div class="accordion__top">
@@ -189,7 +241,7 @@
                   </div>
                 </div>
                 <div class="services__item-right">
-                  <img src="./img/services/3.jpg" alt="картинка услуги" width="808">
+                  <img src="./img/services/4.jpg" alt="картинка услуги" width="808">
                 </div>
               </div>
 
@@ -197,6 +249,7 @@
 
             </div>
           </li>
+          <!-- Торговые сигналы  -->
           <li class="services__item accordion">
 
             <div class="accordion__top">
@@ -238,7 +291,7 @@
                   </div>
                 </div>
                 <div class="services__item-right">
-                  <img src="./img/services/4.jpg" alt="картинка услуги" width="808">
+                  <img src="./img/services/5.jpg" alt="картинка услуги" width="808">
                 </div>
               </div>
 
@@ -246,6 +299,7 @@
 
             </div>
           </li>
+          <!-- Частные консультации  -->
           <li class="services__item accordion">
 
             <div class="accordion__top">
@@ -287,7 +341,7 @@
                   </div>
                 </div>
                 <div class="services__item-right">
-                  <img src="./img/services/5.jpg" alt="картинка услуги" width="808">
+                  <img src="./img/services/6.jpg" alt="картинка услуги" width="808">
                 </div>
               </div>
 
